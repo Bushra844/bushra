58e74c9fd25296af9609efda76ad5331b67d013f
bushra javed <bushiebush0@gmail.com>
2025-07-12 13:01:34 +0500

    Add files via upload

---
+++ membership_operators.py
@@ -0,0 +1,20 @@
+# in
+# not in
+
+s = 'pakistan'
+""" Check presence of a character and string in string, list, tuple and dictionary
+ if 'k1' in s:
+     print("k is present")
+ else:
+     print("k1 is not present")"""
+
+
+l = ['Lahore', 'Dera Ismail Khan', 'Karachi']
+if 'Lahore' in l:
+    print("Present")
+else:
+    print("Not Present")
+
+not 1
+print('Lahore' not in l)
+
